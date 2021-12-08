a8b0840f3dd6d12fa134ed2e3bb60d4cb80bf134
Meng Li <meng.li@signavio.com>
2021-12-08 16:57:59 +0100

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.bundles.testDependencies)
+    implementation(libs.groovy.core)
+    implementation(libs.groovy.json)
+    implementation(libs.groovy.nio)
+
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "gradleDependentest"
+enableFeaturePreview("VERSION_CATALOGS")
+include("app")
+include("core")
+include("app")
+include("core")
